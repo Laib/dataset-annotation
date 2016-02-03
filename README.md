76fcecb8db3f1107575c6bd8d85b1b8de60b0455
William Fu <work.wfu@gmail.com>
2016-02-02 18:56:43 -0800

    Another update

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # dataset-annotation
 
 MATLAB script for annotation image files to create datasets for training. 
+
+# How to use
+
+Run the "annotate.m" file and supply the folder that you want to work in.
+The annotations should be saved in ../Annotations/your-file-name.txt
+
+To make the code fit your dataset, simple modify the files for template.txt
+and objecttemplate.txt. The template.txt is for the header file, and then 
+the objecttemplate.txt part is generated for each object detected in the image.
---
+++ annotate.m
@@ -0,0 +1,40 @@
+%enter the folder or image you want to work on
+input_dir = '/Users/wfu/Desktop/Test';
+working_dir = dir (input_dir);
+
+%need to go to the folder holding image files 
+cd(input_dir);
+
+%stored as a 1xN cell array
+allNames = {working_dir.name};
+
+picture_paths = {};
+picture_fullpaths = {};
+
+
+%uses allnames, 2 because allNames is a 1xN cell array
+for i = 1:size(allNames, 2)
+    filename = char(allNames{i});
+    [pathstr, name, ext] = fileparts(filename);
+   
+    if strcmp(ext, '.png') || strcmp(ext, '.jpg')
+        picture_paths = [picture_paths; {[ '/' filename]}];
+    end
+  
+end
+
+%picture_paths is a Nx1 array
+for i = 1:size(picture_paths, 1)
+    filename = char(picture_paths{i});
+    filename = [input_dir filename];
+    picture_fullpaths = [picture_fullpaths; {filename}];
+end
+
+
+for i = 1:size(picture_fullpaths, 1)
+    fullpath = char(picture_fullpaths{i});
+   get_roi_from_file(fullpath);
+end
+
+
+
